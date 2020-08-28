33c6fb471ad462bb4a8f77951fe0bbf7c12db44c
nicolasraffray <nraffray@gmail.com>
2020-08-28 09:52:24 +0100

    function for pairwise covariance

--- python_classes/lib/metaData.py
+++ python_classes/lib/metaData.py
@@ -9,6 +9,7 @@ class MetaData(DataCollection):
         super().__init__()
         self.normal_returns = pd.DataFrame()
         self.daily_log_returns = None
+        self.pair_covariance = None
 
     def get_normal_returns(self):
         if self.normal_returns.empty:
@@ -19,5 +20,12 @@ class MetaData(DataCollection):
         return self.normal_returns
 
     def get_log_returns(self):
-        self.daily_log_returns = np.log(self.dataFrame/self.dataFrame.shift(1))
+        self.daily_log_returns = np.log(self.dataFrame/self.dataFrame.shift(1)).dropna()
         return self.daily_log_returns
+    
+    def pairwise_covariance(self):
+        if not self.daily_log_returns.empty:
+            cov = self.daily_log_returns.cov() * 252
+            self.pair_covariance = cov
+        else:
+            print("there are no Log Returns to perfrom calc")
--- python_classes/test/test_metaData.py
+++ python_classes/test/test_metaData.py
@@ -8,8 +8,7 @@ from lib.dataCollection import DataCollection
 
 class Test(TestCase):
   meta = MetaData()
-  meta.dataFrame = pd.DataFrame(
-        [452.00, 454.00, 456.00], columns=['FB'])
+  meta.dataFrame = pd.DataFrame(list(zip([452.00, 454.00, 456.00],[35,36,40])), columns=['FB','AB'])
 
   def test_generates_normal_Returns(self):
       self.meta.get_normal_returns()
@@ -17,7 +16,15 @@ class Test(TestCase):
 
   def test_generates_log_returns(self):
     self.meta.get_log_returns()
-    self.assertEqual(round(self.meta.daily_log_returns.iloc[1][0],5),0.00442)
+    self.assertEqual(round(self.meta.daily_log_returns.iloc[1][0],5),0.0044)
+
+  def test_generates_pairwise_covariance(self):
+    self.meta.get_log_returns()
+    self.meta.pairwise_covariance()
+    dataFrame = pd.DataFrame(list(zip([4.745430e-08,-1.887479e-04],[-0.000189,0.750738])), columns=['FB','AB'],index=['FB','AB'])
+    self.assertEqual(round(self.meta.pair_covariance.iloc[0][0],3), round(dataFrame.iloc[0][0],5) )
+
+
 
 
 
