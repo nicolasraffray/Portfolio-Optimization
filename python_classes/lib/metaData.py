8ce414a55d0b42286f9dbdc810d46ae91111a408
nicolasraffray <nraffray@gmail.com>
2020-09-01 19:35:46 +0100

    average daily returns

--- python_classes/lib/metaData.py
+++ python_classes/lib/metaData.py
@@ -20,12 +20,17 @@ class MetaData(DataCollection):
         return self.normal_returns
 
     def get_log_returns(self):
-        self.daily_log_returns = np.log(self.dataFrame/self.dataFrame.shift(1)).dropna()
+        self.daily_log_returns = np.log(
+            self.dataFrame/self.dataFrame.shift(1)).dropna()
         return self.daily_log_returns
-    
+
     def pairwise_covariance(self):
         if not self.daily_log_returns.empty:
             cov = self.daily_log_returns.cov() * 252
             self.pair_covariance = cov
         else:
             print("there are no Log Returns to perfrom calc")
+
+    def average_daily_returns(self):
+        average_daily_return = self.dataFrame.pct_change(1).mean()
+        return average_daily_return
--- python_classes/test/test_metaData.py
+++ python_classes/test/test_metaData.py
@@ -7,27 +7,32 @@ from lib.dataCollection import DataCollection
 
 
 class Test(TestCase):
-  meta = MetaData()
-  meta.dataFrame = pd.DataFrame(list(zip([452.00, 454.00, 456.00],[35,36,40])), columns=['FB','AB'])
+    meta = MetaData()
+    meta.dataFrame = pd.DataFrame(
+        list(zip([452.00, 454.00, 456.00], [35, 36, 40])), columns=['FB', 'AB'])
 
-  def test_generates_normal_Returns(self):
-      self.meta.get_normal_returns()
-      self.assertEqual(round(self.meta.normal_returns.iloc[1][0],5), 1.00442)
+    def test_generates_normal_Returns(self):
+        self.meta.get_normal_returns()
+        self.assertEqual(
+            round(self.meta.normal_returns.iloc[1][0], 5), 1.00442)
 
-  def test_generates_log_returns(self):
-    self.meta.get_log_returns()
-    self.assertEqual(round(self.meta.daily_log_returns.iloc[1][0],5),0.0044)
+    def test_generates_log_returns(self):
+        self.meta.get_log_returns()
+        self.assertEqual(
+            round(self.meta.daily_log_returns.iloc[1][0], 5), 0.0044)
 
-  def test_generates_pairwise_covariance(self):
-    self.meta.get_log_returns()
-    self.meta.pairwise_covariance()
-    dataFrame = pd.DataFrame(list(zip([4.745430e-08,-1.887479e-04],[-0.000189,0.750738])), columns=['FB','AB'],index=['FB','AB'])
-    self.assertEqual(round(self.meta.pair_covariance.iloc[0][0],3), round(dataFrame.iloc[0][0],5) )
+    def test_generates_pairwise_covariance(self):
+        self.meta.get_log_returns()
+        self.meta.pairwise_covariance()
+        dataFrame = pd.DataFrame(list(zip(
+            [4.745430e-08, -1.887479e-04], [-0.000189, 0.750738])), columns=['FB', 'AB'], index=['FB', 'AB'])
+        self.assertEqual(round(self.meta.pair_covariance.iloc[0][0], 3), round(
+            dataFrame.iloc[0][0], 5))
+
+    def test_average_daily_return(self):
+        result = self.meta.average_daily_returns()
+        self.assertEqual(round(result.iloc[0], 5), 0.00442)
 
 
-
-
-
-      
 if __name__ == "__main__":
-  unittest.main()
+    unittest.main()
