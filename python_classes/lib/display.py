742fcecede0149b40e8c0ad0b74f7a7284f1aad3
nicolasraffray <nraffray@gmail.com>
2020-12-31 01:11:38 +0000

    statically calling DataCollection

--- python_classes/lib/display.py
+++ python_classes/lib/display.py
@@ -1,6 +1,7 @@
 from .metaData import MetaData
 from .plotting import Plotting
 from .monteCarlo import MonteCarlo
+from .dataCollection import DataCollection
 
 
 class Display:
@@ -18,7 +19,7 @@ class Display:
         print("\n\n     Hey Hey\n\n")
         while text not in options:
             text = input(
-                'Choose from the following\n \
+                'Choose from the following\n\
                 1) Show Tickers\n\
                 2) Get Tickers\n\
                 3) Portfolio Descriptive Statistics\n\
@@ -28,8 +29,18 @@ class Display:
             elif text == '2':
                 self.ask_for_tickers()
             elif text == '3':
+                DataCollection.get(
+                    self.tickers, 
+                    start=self.start,
+                    end=self.end
+                )
                 self.descriptive_stat_walkthrough()
             elif text == '4':
+                DataCollection.get(
+                    self.tickers, 
+                    start=self.start,
+                    end=self.end
+                )
                 self.monte_carlo_simulation()
 
     # Option 1
@@ -62,15 +73,15 @@ class Display:
 
     # 3a - Descriptive Statistics
     def show_descriptive_stats(self):
-        self.metaData.get(self.tickers, start=self.start,
-                          end=self.end)
-        av_ret, pair_cov, correl = self.metaData.descriptive_statistics()
+        metaData = MetaData()
+        av_ret, pair_cov, correl = metaData.descriptive_statistics()
         print('\n---Average Return---\n\n', av_ret)
         print('\n\n---Pairwise Covariance---\n\n', pair_cov)
         print('\n\n---Correlation Matrix---\n\n', correl)
 
     # 3b - Plotting
     def plotting_walkthrough(self):
+        self.plotting = Plotting()
         options = ['1', '2']
         text = None
         while text not in options:
@@ -78,6 +89,17 @@ class Display:
 
     # 4 - Monte Carlo Simulation
     def monte_carlo_simulation(self):
-        print(self.metaData)
-        monte = MonteCarlo(self.metaData)
-        monte.run_simulation(1000000)
+        monte = MonteCarlo()
+        while True:
+            text = input('\n\nHow many Portfolios would you like to simulate? (Default = 15,000)\n\n')
+            try:
+                if text == '':
+                    num_ports = 15000
+                    break
+                num_ports = int(text)
+                break
+            except:
+                print('Please Enter Numbers Only')
+            
+        print('The simulation is now running.\n')
+        monte.run_simulation(num_ports)
